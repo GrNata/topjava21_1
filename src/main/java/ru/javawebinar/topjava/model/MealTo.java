c20e1d1ce62a65620c2a3a2982993fbad84182f5
GrNata <gr.natalij@gmail.com>
2021-01-12 10:07:14 +0300

    hw1 1.2 optional

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -3,6 +3,8 @@ package ru.javawebinar.topjava.model;
 import java.time.LocalDateTime;
 
 public class MealTo {
+    private final Integer id;
+
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -14,21 +16,23 @@ public class MealTo {
 //    private final Supplier<Boolean> excess;  // filteredByClosure
     private final boolean excess;
 
-    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
+    public MealTo(Integer id, LocalDateTime dateTime, String description, int calories, boolean excess) {
+        this.id = id;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
         this.excess = excess;
     }
 
-//    public Boolean getExcess() {
-//        return excess.get();
-//    }
+    public Integer getId() {
+        return id;
+    }
 
     @Override
     public String toString() {
         return "MealTo{" +
-                "dateTime=" + dateTime +
+                "id=" + id +
+                ", dateTime=" + dateTime +
                 ", description='" + description + '\'' +
                 ", calories=" + calories +
                 ", excess=" + excess +
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -16,6 +16,8 @@
     <h3><a href="index.html">Home</a> </h3>
     <hr/>
     <h2>Meals</h2>
+    <a href="meals?action=create">Add Meal</a>
+    <br><br>
 
     <table border="1" cellpadding="8" cellspacing="0">
         <thead>
@@ -23,15 +25,19 @@
             <th>Data</th>
             <th>Description</th>
             <th>Calories</th>
+            <th></th>
+            <th></th>
         </tr>
         </thead>
         <tr>
-            <c:forEach var="meal" items="${mealTos}">
+            <c:forEach var="meal" items="${meals}">
             <jsp:useBean id="meal" type="ru.javawebinar.topjava.model.MealTo"/>
                 <tr class="${meal.excess ? 'excess' : 'normal'}">
                 <td>${fn:formatDateTime(meal.dateTime)}</td>
                 <td>${meal.description}</td>
                 <td>${meal.calories}</td>
+                <td><a href="meals?action=update&id=${meal.id}">Update</a> </td>
+                <td><a href="meals?action=delete&id=${meal.id}">Delete</a> </td>
                 </tr>
             </c:forEach>
         </tr>
