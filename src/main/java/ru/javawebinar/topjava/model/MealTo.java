34ad5803eba14c1050fd3e950235011f0b5a6ae1
GrNata <gr.natalij@gmail.com>
2021-01-12 13:01:35 +0300

    hw1 1.2 optional

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -26,6 +26,26 @@ public class MealTo {
 //        return excess.get();
 //    }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExcess() {
+        return excess;
+    }
+
     @Override
     public String toString() {
         return "MealTo{" +
